037db4f3c74151e878b79034ef96c278da31926a
lolqillz <darrelgoh07@gmail.com>
2020-06-19 12:09:52 +0800

    Created PA11 by Darrel

---
+++ 15_Darrel_PA11/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoretext;
+    private int updatedscore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updatedscore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    
+    public void AddScore(int score)
+    {
+        updatedscore += score;
+        scoretext.text = "Score: " + updatedscore;
+    }
+}
---
+++ 15_Darrel_PA11/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject1;
+    public GameObject SpawnObject2;
+    public GameObject SpawnObject3;
+    int SelectObject;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        SelectObject = Random.Range(1, 4);
+        switch (SelectObject)
+        {
+            case 1:
+                Instantiate(SpawnObject1, transform.position, transform.rotation);
+                break;
+            case 2:
+                Instantiate(SpawnObject2, transform.position, transform.rotation);
+                break;
+            case 3:
+                Instantiate(SpawnObject3, transform.position, transform.rotation);
+                break;
+        }
+    }
+}
